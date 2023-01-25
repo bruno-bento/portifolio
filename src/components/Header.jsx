ecc2dfa94cc32a265053f659a5ca406035c90c34
bruno-bento <brunoreinoso10@gmail.com>
2023-01-25 15:39:38 -0300

    incremento do curriculo e alteração em textos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import styles from './Header.module.css'
+import React, { useState } from 'react';
+import Curriculum from '../assets/BrunoBento_CV.pdf'
 
-function Header({setShowDefaultDescription}) {
+function Header({ setShowDefaultDescription }) {
+    const [downloading, setDownloading] = useState(false);
+
+
     function handleDefaultDescription() {
         setShowDefaultDescription(true)
     }
     return (
         <header className={styles.header}>
-            <nav>
+            <nav className={styles.nav}>
                 <p onClick={handleDefaultDescription} ><span>Bruno</span> Bento</p>
+                <a href={Curriculum} download="BrunoBento_CV" target='_blank' className={styles.btn}>
+                    Currículo
+                </a>
             </nav>
         </header>
     )
